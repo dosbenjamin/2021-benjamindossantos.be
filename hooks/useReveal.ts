7ff7bdf26561c50417785986cc8242c4531bbefe
Benjamin Dos Santos <dosbenjamin@gmail.com>
2021-07-24 18:28:18 +0200

    refactor(useReveal): change translate animation value

--- hooks/useReveal.ts
+++ hooks/useReveal.ts
@@ -7,7 +7,7 @@ export const useReveal = () => {
     setReveal,
     revealClass: `transition-transform-opacity duration-1000 ease-out origin-bottom-left transform-gpu ${reveal
         ? 'opacity-100 translate-y-0 rotate-0'
-        : 'opacity-0 translate-y-[25%] rotate-1'
+        : 'opacity-0 translate-y-4 rotate-1'
       }`
   }
 }
