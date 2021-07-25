d804d5b07c13a9bf09dc605f37be16c224244471
Benjamin Dos Santos <dosbenjamin@gmail.com>
2021-07-25 15:28:53 +0200

    refactor(useReveal): add animation when reveal is true

--- hooks/useReveal.ts
+++ hooks/useReveal.ts
@@ -5,8 +5,8 @@ export const useReveal = () => {
 
   return {
     setReveal,
-    revealClass: `transition-transform-opacity duration-1000 ease-out origin-bottom-left transform-gpu ${reveal
-        ? 'opacity-100 translate-y-0 rotate-0'
+    revealClass: `duration-1000 ease-out origin-bottom-left transform-gpu ${reveal
+        ? 'transition-transform-opacity opacity-100 translate-y-0 rotate-0'
         : 'opacity-0 translate-y-4 rotate-1'
       }`
   }
