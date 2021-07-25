b69d76ba3c0bf8b19fc2d907c83d87c7ed384c98
Benjamin Dos Santos <dosbenjamin@gmail.com>
2021-07-25 15:25:31 +0200

    feat: add favicon and manifest

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,20 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <main className="w-screen h-screen p-2">
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/manifest.webmanifest" />
+        <meta name="theme-color" content="#E8E8E8" />
+      </Head>
       <Component {...pageProps} />
-    </main>
+    </>
   )
 }
 
